37cd9cb19aed946546f94470886aa3a81e767dd5
Andrea <andrea.belluccia@gmail.com>
2019-08-29 17:45:58 +0200

    Limpiado los Modulos

--- src/app/app-routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { appRoutes } from "./app.routes";
-import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
-
-@NgModule({
-  imports: [RouterModule.forRoot(appRoutes, { useHash: true })],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,13 @@
-import { RegisterComponent } from "./login/register.component";
-import { PagesComponent } from "./pages/pages.component";
-import { PagenotfoundComponent } from "./shared/pagenotfound/pagenotfound.component";
-import { Routes } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 
-import { ProgressComponent } from "./pages/progress/progress.component";
 import { LoginComponent } from "./login/login.component";
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
-import { Graficas1Component } from "./pages/graficas1/graficas1.component";
+import { RegisterComponent } from "./login/register.component";
+import { PagenotfoundComponent } from "./shared/pagenotfound/pagenotfound.component";
 
-export const appRoutes: Routes = [
-  {
-    path: "",
-    component: PagesComponent,
-    children: [
-      { path: "dashboard", component: DashboardComponent },
-      { path: "progress", component: ProgressComponent },
-      { path: "graficas1", component: Graficas1Component },
-      { path: "", redirectTo: "/dashboard", pathMatch: "full" }
-    ]
-  },
+const appRoutes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
   { path: "**", component: PagenotfoundComponent }
 ];
+
+export const APP_ROUTES = RouterModule.forRoot(appRoutes, { useHash: true });
